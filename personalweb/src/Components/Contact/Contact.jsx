6a9c05e4745030a8620681f278ad2a3fc008583d
Andrew Lieu <andrewlieu@airbears2-10-142-169-65.airbears2.1918.berkeley.edu>
2019-09-11 23:37:45 -0700

    omg

---
+++ personalweb/src/Components/Contact/Contact.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./Contact.css";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="laser"></div>
+        <div className="spaceship"></div>
+        <div className="spaceship1 "></div>
+        <div className="spaceship2"></div>
+
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+          rel="stylesheet"
+        ></link>
+        <div className="title"> CONTACT </div>
+        <div className="describe delay0">email: andrewlieu@berkeley.edu</div>
+      </div>
+    );
+  }
+}
+export default Contact;
